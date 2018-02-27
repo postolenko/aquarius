b490453054b5acd445d0c7a65809c54aca07e9ef
postolenko <denis.postolenko@gmail.com>
2018-02-27 02:56:34 +0200

    Aquarius

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,6 @@
-var w = window,
-d = document,
-e = d.documentElement,
-g = d.getElementsByTagName('body')[0],
-bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
-
 $(window).load(function() {
 
-    getNavItemsSize();
+    // getNavItemsSize();
 
     getHeaderStyles();
 
@@ -29,8 +23,6 @@ $(document).scroll(function() {
 $(document).ready(function() {
    
     getNavItemsSize();
-
-    getHeaderStyles();
 
 });
 
